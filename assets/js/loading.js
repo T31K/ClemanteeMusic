fee25387ceb4c157bbc6cf13b13db627b044e5bc
T31K <t31kmunwong@gmail.com>
2020-10-30 15:15:37 +0800

    Added preloader component

---
+++ assets/js/loading.js
@@ -0,0 +1,15 @@
+// var loading = document.getElementById("loading")
+
+// function hideLoader(){
+//     loading.style.opcaity = "0";
+// }
+
+function myFunction() {
+    var x = document.getElementById("loading");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
